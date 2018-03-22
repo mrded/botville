b848f0fa9ce576e288eaf0ff63f3fe5403b42ab8
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-03-22 00:47:15 +0000

    Implement login.

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const page = require('webpage').create();
 const Promise = require('es6-promise').Promise;
 const system = require('system');
 
-const username = system.env.USERNAME;
-const password = system.env.PASSWORD;
-
-if (!username || !password) {
+if (!system.env.USERNAME || !system.env.PASSWORD) {
   throw 'USERNAME or PASSWORD variables are missing.';
 }
+
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
 const openPromise = new Promise(function(resolve, reject) {
   console.log(':: Open website');
 
-  const data = 'username=' + username + '&password=' + password;
+  const data = "username=:username&password=:password&commit=Войти!"
+    .replace(':username', system.env.USERNAME)
+    .replace(':password', system.env.PASSWORD);
 
-  page.open('https://godville.net/login/login', 'post', data, function(status) {
+  const url = 'https://godville.net/login/login'; // Russian.
+  // const url = 'https://godvillegame.com/login/login'; // English.
+
+  page.open(url, 'post', data, function(status) {
     status === 'success' 
       ? resolve() 
       : reject(new Error('Problems while opening a website'));
